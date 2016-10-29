670b4400923c3cb1432e53fbe24afaf5a8d17878
Vsattce <584153255@qq.com>
2016-10-29 17:15:38 +0800

    initialize project

---
+++ Assets/JCCode/Demo/Scripts/ObjectPoolDemo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using JCCode;
+
+public class ObjectPoolDemo : MonoBehaviour 
+{
+    public RandomCube randomCube;
+    public RandomSphere randomSphere;
+
+    void Update()
+    {
+        randomCube.Spawn(Vector3.zero);
+
+        randomSphere.Spawn(Vector3.one * Random.Range(-2.0f, 2.0f));
+    }
+}
---
+++ Assets/JCCode/Demo/Scripts/RandomCube.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using JCCode;
+
+public class RandomCube : MonoBehaviour 
+{
+    float _t = 3.0f;
+    float _timer = 0.0f;
+
+    void OnEnable()
+    {
+        _timer = 0.0f;
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _t)
+        {
+            gameObject.Recycle();
+        }
+    }
+}
