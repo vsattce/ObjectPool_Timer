64db191d9598919b4608eca2a57f72eb297b6b8a
TTTT <584153255@qq.com>
2017-04-09 18:44:33 +0800

    update object pool

---
+++ Assets/JCCode/ObjectPool/Demo/Scripts/ObjectPoolDemo1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace JCCode
+{
+    namespace ObjectPool
+    {
+        public class ObjectPoolDemo1 : MonoBehaviour
+        {
+            public RandomCube randomCube;
+            public RandomSphere randomSphere;
+
+            private float _delayTime = 2.0f;
+            private float _timer = 0.0f;
+
+            void Update()
+            {
+                _timer += Time.deltaTime;
+                if (_timer > _delayTime)
+                {
+                    randomCube.Spawn(Vector3.one);
+
+                    randomSphere.Spawn(Vector3.one * 2.0f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/JCCode/ObjectPool/Demo/Scripts/RandomSphere.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace JCCode
+{
+    namespace ObjectPool
+    {
+        public class RandomSphere : MonoBehaviour
+        {
+            float _t = 1.0f;
+            float _timer = 0.0f;
+
+            void OnEnable()
+            {
+                _timer = 0.0f;
+            }
+
+            void Update()
+            {
+                _timer += Time.deltaTime;
+                if (_timer > _t)
+                {
+                    gameObject.Recycle();
+                }
+            }
+        }
+    }
+}
